c6713d50c3a08e556cf7ebc644511ca32c5164e4
Jena <jenagansivakumar@gmail.com>
2024-01-31 15:26:46 +0100

    creating balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Header } from "./components/header";
+import { Balance } from "./components/balance";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
